682bea97bb90569cdac28697b309271b62f181f8
Stellacore <dave@stellacore.com>
2024-11-06 09:49:45 -0700

    include/datGrid.hpp - raster data storage - with working test

---
+++ include/datGrid.hpp
@@ -0,0 +1,516 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Stellacore Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+#pragma once
+
+
+/*! \file
+ * \brief Definitions for dat::Grid<> raster data structure
+ *
+ */
+
+
+#include "datExtents.hpp"
+
+#include <string>
+
+// implementation
+#include <stdio.h>
+#include <sstream>
+
+
+namespace quadloco
+{
+
+namespace dat
+{
+	struct RowCol
+	{
+		std::size_t const theHigh{ 0u };
+		std::size_t const theWide{ 0u };
+
+	}; // RowCol
+
+	/*! \brief Holds typed data in layout with row/cols.
+
+	Data values are stored in row major order.
+	Iterations proceed as (0, 0), (0, 1), (0, 2) etc. (along rows)
+
+	\par Example
+	\snippet test/test_datGrid.cpp DoxyExample01
+	*/
+	template < typename Type >
+	class Grid
+	{
+		std::size_t theHigh{ 0u }; //!< Height of buffer
+		std::size_t theWide{ 0u }; //!< Width of buffer
+		Type * theData{ nullptr }; //!< data buffer pointer
+
+	public: // typedef
+
+		typedef Type value_type;
+		typedef Type * iterator;
+		typedef Type const * const_iterator;
+		typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+		typedef std::reverse_iterator<iterator> reverse_iterator;
+
+	public: // static methods
+
+		//! A null (zero sized) grid
+		static
+		Grid
+		null
+			()
+		{
+			return {};
+		}
+
+		// disable copy construction
+		Grid
+			(Grid const & other) = delete;
+
+		// disable copy assignment
+		Grid &
+		operator=
+			(Grid const & rhs) = delete;
+
+	public: // methods
+
+		//! Construct empty
+		Grid
+			() = default;
+
+		//! Construct with geometry (NOTE: uninitialized values)
+		inline
+		explicit
+		Grid
+			( Extents const & hwArea
+			)
+			: theHigh{ hwArea.high() }
+			, theWide{ hwArea.wide() }
+			, theData{ nullptr }
+		{
+			if (0 < theHigh && 0 < theWide)
+			{
+				theData = new Type[theHigh * theWide];
+			}
+		}
+
+		//! Move constructor
+		inline
+		Grid
+			( Grid<Type> && orig
+			)
+			: Grid()
+		{
+			std::swap(theHigh, orig.theHigh);
+			std::swap(theWide, orig.theWide);
+			std::swap(theData, orig.theData);
+		}
+
+		//! Move assignment
+		inline
+		Grid<Type> &
+		operator=
+			( Grid<Type> && rhs
+			)
+		{
+			if (&rhs != this)
+			{
+				std::swap(theHigh, rhs.theHigh);
+				std::swap(theWide, rhs.theWide);
+				std::swap(theData, rhs.theData);
+			}
+			return *this;
+		}
+
+		//! standard destructor
+		inline
+		~Grid
+			()
+		{
+			if (theData)
+			{
+				delete [] theData;
+			}
+		}
+
+		//! check if is valid
+		inline
+		bool
+		isValid
+			() const
+		{
+			return theData != nullptr;
+		}
+
+		//! Dimensions of this image
+		inline
+		Extents
+		hwArea
+			() const
+		{
+			return Extents(theHigh, theWide);
+		}
+
+		//! Number of cells in grid
+		inline
+		std::size_t
+		size
+			() const
+		{
+			return hwArea().size();
+		}
+
+		//! bytes in buffer
+		inline
+		size_t
+		byteSize
+			() const
+		{
+			return hwArea().size() * sizeof(Type);
+		}
+
+		//! Returns reference to element
+		inline
+		Type const &
+		operator()
+			( size_t const & row
+			, size_t const & col
+			) const
+		{
+			return *(theData + row * theWide + col);
+		}
+
+		//! Returns pointer to row (non const)
+		inline
+		Type &
+		operator()
+			( size_t const & row
+			, size_t const & col
+			)
+		{
+			return *(theData + row * theWide + col);
+		}
+
+		//! Returns reference to element
+		inline
+		Type const &
+		operator()
+			( dat::RowCol const & rowcol
+			) const
+		{
+			return operator()(rowcol.theHigh, rowcol.theWide);
+		}
+
+		//! Returns pointer to row (non const)
+		inline
+		Type &
+		operator()
+			( dat::RowCol const & rowcol
+			)
+		{
+			return operator()(rowcol.theHigh, rowcol.theWide);
+		}
+
+		//! use as const_iterator
+		inline
+		const_iterator
+		cbegin
+			() const
+		{
+			return theData;
+		}
+
+		//! use as const_iterator
+		inline
+		const_iterator
+		cend
+			() const
+		{
+			return theData + hwArea().size();
+		}
+
+		//! use as iterator
+		inline
+		iterator
+		begin
+			()
+		{
+			return theData;
+		}
+
+		//! use as iterator
+		inline
+		iterator
+		end
+			()
+		{
+			return theData + hwArea().size();
+		}
+
+		//! use as const_reverse_iterator
+		inline
+		const_reverse_iterator
+		crbegin
+			() const
+		{
+			return const_reverse_iterator(end());
+		}
+
+		//! use as const_reverse_iterator
+		inline
+		const_reverse_iterator
+		crend
+			() const
+		{
+			return const_reverse_iterator(begin());
+		}
+
+		//! use as reverse_iterator
+		inline
+		reverse_iterator
+		rbegin
+			()
+		{
+			return reverse_iterator(end());
+		}
+
+		//! use as reverse_iterator
+		inline
+		reverse_iterator
+		rend
+			()
+		{
+			return reverse_iterator(begin());
+		}
+
+		//! const row access, use as const iterator
+		inline
+		const_iterator
+		cbeginRow
+			( size_t const & row
+			) const
+		{
+			return theData + (row * theWide);
+		}
+
+		//! const row access, use as const iterator
+		inline
+		const_iterator
+		cendRow
+			( size_t const & row
+			) const
+		{
+			return beginRow(row + 1);
+		}
+
+		//! row access, use as iterator
+		inline
+		iterator
+		beginRow
+			( size_t const & row
+			)
+		{
+			return theData + (row * theWide);
+		}
+
+		//! row access, use as iterator
+		inline
+		iterator
+		endRow
+			( size_t const & row
+			)
+		{
+			return beginRow(row + 1);
+		}
+
+		//! iterator at given row, ocl
+		inline
+		const_iterator
+		citerAt
+			( size_t const & row
+			, size_t const & col
+			) const
+		{
+			return theData + (row * theWide + col);
+		}
+
+		//! iterator at given row, ocl
+		inline
+		iterator
+		iterAt
+			( size_t const & row
+			, size_t const & col
+			)
+		{
+			return theData + (row * theWide + col);
+		}
+
+		//! row/col from iterator
+		inline
+		dat::RowCol
+		rowColFor
+			( const_iterator const & iter
+			) const
+		{
+			size_t const dist(std::distance(begin(), iter));
+			return dat::RowCol
+				{ dist / hwArea().wide()
+				, dist % hwArea().wide()
+				};
+		}
+
+		/*! \brief Descriptive information about this instance.
+		 *
+		 * Format is:
+		 * \arg title "High,Wide" high wide "Cells,Bytes" ncells nbytes
+		 */
+		inline
+		std::string
+		infoString
+			( std::string const & title=std::string()
+			) const
+		{
+			std::ostringstream os;
+			if (!title.empty())
+			{
+				os << title << " ";
+			}
+			Extents const hw{ hwArea() };
+			os << "High,Wide:"
+				<< ' ' << std::setw(5) << hw.high()
+				<< ' ' << std::setw(5) << hw.wide()
+				<< "  Cells,Bytes:"
+				<< ' ' << std::setw(5) << size()
+				<< ' ' << std::setw(5) << byteSize()
+				;
+			return os.str();
+		}
+
+		/*! \brief The content values of the this instance
+		 *
+		 * Format is:
+		 * \arg infoString() as first line
+		 * \arg each line is a row of values formatted with cfmt
+		 */
+		inline
+		std::string
+		infoStringContents
+			( std::string const & title
+				//!< Heading to print (along with size info)
+			, std::string const & cfmt
+				//!< A 'printf' style string to format each cell value
+			) const
+		{
+			std::ostringstream oss;
+			oss << infoString(title);
+			if (isValid())
+			{
+				const_iterator iter{ cbegin() };
+				for (size_t row(0); row < theHigh; ++row)
+				{
+					oss << '\n';
+					for (size_t col(0); col < theWide; ++col)
+					{
+						constexpr std::size_t bufSize{ 1024u };
+						char buf[bufSize+1u]; // hopefully large enough
+						std::snprintf(buf, bufSize, cfmt.c_str(), *iter++);
+						oss << ' ' << buf;
+					}
+				}
+			}
+			else
+			{
+				oss << " <null>";
+			}
+			return oss.str();
+		}
+
+/*
+		//! exactly equals operator
+		inline
+		bool
+		operator==
+			( Grid const & rhs
+			) const;
+
+		//! Cast to different element type
+		template <typename OutType>
+		inline
+		dat::Grid<OutType>
+		castGrid
+			() const;
+
+		//! Number of valid column entires in specified row
+		inline
+		size_t
+		validCountInRow
+			( size_t const & rowNdx
+			) const;
+
+		//! Number of valid rows entires in specified column
+		inline
+		size_t
+		validCountInCol
+			( size_t const & colNdx
+			) const;
+*/
+
+/*
+		//! destructive resize
+		inline
+		void
+		resize
+			( size_t const & high
+			, size_t const & wide
+			);
+*/
+
+	};
+
+
+} // [dat]
+
+} // [quadloco]
+
+
+namespace
+{
+	//! Put obj.infoString() to stream
+	template < typename Type >
+	inline
+	std::ostream &
+	operator<<
+		( std::ostream & ostrm
+		, quadloco::dat::Grid<Type> const & obj
+		)
+	{
+		ostrm << obj.infoString();
+		return ostrm;
+	}
+
+} // [anon/global]
+
---
+++ test/test_datGrid.cpp
@@ -0,0 +1,158 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Stellacore Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+/*! \file
+\brief Unit tests (and example) code for quadloco::dat::Grid
+*/
+
+
+#include "datGrid.hpp"
+
+#include "QuadLoco"
+
+#include <algorithm>
+#include <functional>
+#include <iostream>
+#include <numeric>
+#include <sstream>
+
+
+namespace
+{
+	//! Examples for documentation
+	void
+	test0
+		( std::ostream & oss
+		)
+	{
+		// [DoxyExample01]
+
+		// general raster grid storage and access - tiny image example
+		quadloco::dat::Extents const hwArea{ 2u, 3u };
+		quadloco::dat::Grid<float> grid(hwArea);
+
+		// use iterators to fill values
+		constexpr float fillVal{ 17.f/8.f };
+		std::fill(grid.begin(), grid.end(), fillVal);
+
+		// set a couple cell values explicitly
+		constexpr float dVal{ 2.5f };
+		using quadloco::dat::RowCol;
+		grid(RowCol{ 0u, 0u }) = dVal;
+		grid(1u, 1u) = dVal;
+		grid(0u, 2u) = grid(0u, 0u);
+
+		// return move copy
+		using FGrid = quadloco::dat::Grid<float>;
+		std::function<FGrid(FGrid const &)> const copyFunc
+			{ [](FGrid const & orig)
+				{
+				FGrid copy(orig.hwArea());
+				std::copy(orig.cbegin(), orig.cend(), copy.begin());
+				return std::move(copy); // move assign
+				}
+			};
+		FGrid const copy{ copyFunc(grid) };
+
+		// constant iterator access
+		float const gotSum
+			{ std::accumulate(copy.cbegin(), copy.cend(), 0.f) };
+		float const expSum{ 3.f*dVal + 3.*fillVal };
+
+		// output
+		std::ostringstream ostrm;
+		ostrm << grid << '\n'; // puts grid.infoString() to stream
+		// put grid cell contents to stream
+		ostrm << grid.infoStringContents("gridValues\n", "%9.3f") << '\n';
+
+		// [DoxyExample01]
+
+		if (! (gotSum == expSum)) // exact given binary exact test values.
+		{
+			oss << "Failure of grid sum test\n";
+			oss << "expSum: " << expSum << '\n';
+			oss << "gotSum: " << gotSum << '\n';
+			oss << "grid: " << grid << '\n';
+			oss << copy.infoStringContents("copy\n", "%6.3f") << '\n';
+		}
+
+		// exercise i/o functions
+
+		// save contents to stream
+		std::ostringstream save;
+		save << grid.infoStringContents("", "%9.3f") << '\n';
+		// load from stream (here into a double value grid)
+		std::istringstream iss{ save.str() };
+		std::string txt;
+		std::size_t high, wide;
+		std::size_t nc, nb;
+		iss >> txt;  // skip "High,Wide" label
+		iss >> high >> wide;
+		iss >> txt;  // skip "Cells,Bytes" label
+		iss >> nc >> nb;  // skip cell and byte size values
+		quadloco::dat::Extents const loadHW{ high, wide };
+		quadloco::dat::Grid<double> load(loadHW); // create data container
+		quadloco::dat::Grid<double>::iterator inIter{ load.begin() };
+		for (std::size_t nCell{0u} ; nCell < load.size() ; ++nCell)
+		{
+			iss >> *inIter++;
+		}
+
+		bool const loadOkay
+			{ std::equal(grid.cbegin(), grid.cend(), load.cbegin()) };
+		if (! loadOkay)
+		{
+			oss << "Failure of save/load test\n";
+			oss << grid.infoStringContents("grid\n", "%9.3f") << '\n';
+			oss << load.infoStringContents("load\n", "%9.3f") << '\n';
+		}
+
+	}
+
+}
+
+//! Check behavior of NS
+int
+main
+	()
+{
+	int status{ 1 };
+	std::stringstream oss;
+
+	test0(oss);
+
+	if (oss.str().empty()) // Only pass if no errors were encountered
+	{
+		status = 0;
+	}
+	else
+	{
+		// else report error messages
+		std::cerr << "### FAILURE in test file: " << __FILE__ << std::endl;
+		std::cerr << oss.str();
+	}
+	return status;
+}
+
