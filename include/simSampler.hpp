172a2178709e8274977b9a88b0d8006519e26492
Stellacore <dave@stellacore.com>
2024-11-08 14:53:13 -0700

    include/simSampler.hpp - invert order of supersampling and noise compuation

--- include/simSampler.hpp
+++ include/simSampler.hpp
@@ -152,21 +152,81 @@ namespace sim
 		//! Sample theObjQuad near forward projection of detector location
 		inline
 		double
-		intensityAt
+		quadSignalFor
 			( dat::Spot const & detSpot
 			) const
 		{
 			double intenSample{ engabra::g3::null<double>() };
 
 			engabra::g3::Vector const pntInQuad{ quadLocFor(detSpot) };
+			if (isValid(pntInQuad))
+			{
+				// sample intensity from quad target
+				dat::Spot const spotInQuad{ pntInQuad[0], pntInQuad[1] };
 
-			// sample intensity from quad target
-			dat::Spot const spotInQuad{ pntInQuad[0], pntInQuad[1] };
+				// target signal intensity
+				intenSample = theObjQuad.intensityAt(spotInQuad);
+			}
 
-			// target signal intensity
-			double const valueSignal{ theObjQuad.intensityAt(spotInQuad) };
+			// return the sample value
+			return intenSample;
+		}
+
+		//! Use subsampling with mutiple calls to intensityAt()
+		inline
+		double
+		pureSignalIntensity
+			( dat::Spot const & detSpot
+			, std::size_t const & numSubSamps
+			) const
+		{
+			double intensity{ engabra::g3::null<double>() };
+			static std::mt19937 gen(48997969u);
+			static std::normal_distribution<double> distro(-.75, .75);
+			double sum{ 0. };
+			double count{ 0. };
+			for (std::size_t nn{0u} ; nn < numSubSamps ; ++nn)
+			{
+				dat::Spot const delta{ distro(gen), distro(gen) };
+				dat::Spot const useSpot{ detSpot + delta };
+				if (isValid(useSpot))
+				{
+					sum += quadSignalFor(useSpot);
+					count += 1.;
+				}
+			}
+			if (0. < count)
+			{
+				intensity = (1./count) * sum;
+			}
+			return intensity;
+		}
+
+		//! Use subsampling with mutiple calls to intensityAt()
+		inline
+		double
+		intensityNear
+			( dat::Spot const & detSpot
+			, std::size_t const & numSubSamps = 16u
+			) const
+		{
+			double intenSample{ engabra::g3::null<double>() };
+
+			// initial supersampled ideal signal
+			double const valueSignal
+				{ pureSignalIntensity(detSpot, numSubSamps) };
+
+			// add noise
 			if (engabra::g3::isValid(valueSignal))
 			{
+				// since there is some signal, the detSpot must be
+				// close to the quad target boundary.  I.e., intersection
+				// with quad plane location may be just outside the
+				// quad boundary, but is close enough to use for scene
+				// illumination bias
+				engabra::g3::Vector const pntInQuad{ quadLocFor(detSpot) };
+				dat::Spot const spotInQuad{ pntInQuad[0], pntInQuad[1] };
+
 				// illumination bias across target
 				double const valueBias{ noiseBias(spotInQuad) };
 
@@ -178,41 +238,10 @@ namespace sim
 					{ theNoiseModel.valueFor(incidentValue) };
 
 				// final recorded pixel value
-				intenSample = (valueSignal + valueBias + valueNoise);
+				intenSample = incidentValue + valueNoise;
 			}
 
-			// return the sample value
 			return intenSample;
-		}
-
-		//! Use subsampling with mutiple calls to intensityAt()
-		inline
-		double
-		intensityNear
-			( dat::Spot const & detSpot
-			, std::size_t const & numSubSamps = 16u
-			) const
-		{
-			double intensity{ engabra::g3::null<double>() };
-			static std::mt19937 gen(48997969u);
-			static std::uniform_real_distribution<double> distro(-.75, .75);
-			double sum{ 0. };
-			double count{ 0. };
-			for (std::size_t nn{0u} ; nn < numSubSamps ; ++nn)
-			{
-				dat::Spot const useSpot
-					{ detSpot + dat::Spot{ distro(gen), distro(gen) } };
-				if (isValid(useSpot))
-				{
-					sum += intensityAt(useSpot);
-					count += 1.;
-				}
-			}
-			if (0. < count)
-			{
-				intensity = (1./count) * sum;
-			}
-			return intensity;
 		}
 
 	}; // Sampler
