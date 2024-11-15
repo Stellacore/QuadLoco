4f3305abb6331db65eb6dc337fc621babbddc2c8
Stellacore <dave@stellacore.com>
2024-11-14 17:04:52 -0700

    include/fndParmAD.hpp - initial version ParmAD (factor from test_fndHoughAD.cpp)

---
+++ include/fndParmAD.hpp
@@ -0,0 +1,241 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Stellacore Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+#pragma once
+
+
+/*! \file
+ * \brief TODO
+ *
+ */
+
+
+#include "datCircle.hpp"
+#include "datSpot.hpp"
+#include "pixEdgel.hpp"
+
+#include <Engabra>
+
+#include <iostream>
+#include <utility>
+
+
+namespace quadloco
+{
+
+namespace fnd
+{
+
+	/*! Hough line space "alpha,delta" parameter representation
+	 *
+	 * For description, refer to theory/HoughModel.lyx
+	 */
+	struct ParmAD
+	{
+		//! Angle position of line segment start point
+		double const theAlpha{ engabra::g3::null<double>() };
+
+		//! Difference in angular position of line end point from start point
+		double const theDelta{ engabra::g3::null<double>() };
+
+
+		//! Double precision vector from pix::Spot
+		inline
+		static
+		dat::Vec2D<double>
+		linePntFromEdgeLoc
+			( pix::Spot const & spot
+			)
+		{
+			return dat::Vec2D<double>
+				{ (double)spot[0]
+				, (double)spot[1]
+				};
+		}
+
+		//! Direction perpendicular to gradient direction in right hand sense
+		inline
+		static
+		dat::Vec2D<double>
+		lineDirFromEdgeDir
+			( pix::Grad const & grad
+			)
+		{
+			return dat::Vec2D<double>
+				{ -(double)grad[1]
+				,  (double)grad[0]
+				};
+		}
+
+		//! Angle (from circle center) to line seg start on cicle
+		inline
+		static
+		double
+		alphaFor
+			( dat::Spot const & spotOnCircle
+			, dat::Circle const & circle
+			)
+		{
+			double const dx{ spotOnCircle[0] - circle.theCenter[0] };
+			double const dy{ spotOnCircle[1] - circle.theCenter[1] };
+			double const alpha{ std::atan2(dy, dx) };
+			return alpha;
+		}
+
+		//! Angle (from circle center) to line seg end on cicle
+		inline
+		static
+		double
+		deltaFor
+			( dat::Spot const & spotOnCircle
+			, dat::Circle const & circle
+			, double const & alpha
+			)
+		{
+			double const dx{ spotOnCircle[0] - circle.theCenter[0] };
+			double const dy{ spotOnCircle[1] - circle.theCenter[1] };
+			double const delta{ (std::atan2(dy, dx) - alpha) };
+			return delta;
+		}
+
+		inline
+		static
+		ParmAD
+		from
+			( pix::Edgel const & edgel
+			, dat::Circle const & circle
+			)
+		{
+			// compute intersection points on circle
+			dat::CircleIntersector const intersector{ circle };
+			std::pair<dat::Spot, dat::Spot> const solnPair
+				{ intersector
+					( linePntFromEdgeLoc(edgel.location())
+					, lineDirFromEdgeDir(edgel.gradient())
+					)
+				};
+
+			// compute alpha,delta values for intersection points
+			double const alpha{ alphaFor(solnPair.first, circle) };
+			double const delta{ deltaFor(solnPair.second, circle, alpha) };
+			ParmAD const parmAD{ alpha, delta };
+
+			return parmAD;
+		}
+
+		//! True if this instance is valid
+		inline
+		bool
+		isValid
+			() const
+		{
+			return
+				(  engabra::g3::isValid(theAlpha)
+				&& engabra::g3::isValid(theDelta)
+				);
+		}
+
+		//! True if this instance is nearly the same as other within tol
+		inline
+		bool
+		nearlyEquals
+			( ParmAD const & other
+			, double const & tol = std::numeric_limits<double>::epsilon()
+			) const
+		{
+			return
+				(  engabra::g3::nearlyEquals(theAlpha, other.theAlpha, tol)
+				&& engabra::g3::nearlyEquals(theDelta, other.theDelta, tol)
+				);
+		}
+
+		//! Descriptive information about this instance.
+		inline
+		std::string
+		infoString
+			( std::string const & title = {}
+			) const
+		{
+			std::ostringstream oss;
+			if (! title.empty())
+			{
+				oss << title << ' ';
+			}
+			using engabra::g3::io::fixed;
+			oss
+				<< "theAlpha: " << fixed(theAlpha)
+				<< ' '
+				<< "theDelta: " << fixed(theDelta)
+				;
+
+			return oss.str();
+		}
+
+	}; // ParmAD
+
+
+} // [fnd]
+
+} // [quadloco]
+
+
+namespace
+{
+	//! Put item.infoString() to stream
+	inline
+	std::ostream &
+	operator<<
+		( std::ostream & ostrm
+		, quadloco::fnd::ParmAD const & item
+		)
+	{
+		ostrm << item.infoString();
+		return ostrm;
+	}
+
+	//! True if item is not null
+	inline
+	bool
+	isValid
+		( quadloco::fnd::ParmAD const & item
+		)
+	{
+		return item.isValid();
+	}
+
+	//! True if both items have very nearly the same values
+	inline
+	bool
+	nearlyEquals
+		( quadloco::fnd::ParmAD const & itemA
+		, quadloco::fnd::ParmAD const & itemB
+		, double const & tol = std::numeric_limits<double>::epsilon()
+		)
+	{
+		return itemA.nearlyEquals(itemB, tol);
+	}
+
+} // [anon/global]
+
---
+++ test/test_fndParmAD.cpp
@@ -0,0 +1,143 @@
+//
+// MIT License
+//
+// Copyright (c) 2024 Stellacore Corporation
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+//
+
+
+/*! \file
+\brief Unit tests (and example) code for quadloco::fnd::ParmAD
+*/
+
+
+#include "fndParmAD.hpp"
+
+#include <iostream>
+#include <sstream>
+
+
+namespace
+{
+	//! Examples for documentation
+	void
+	test0
+		( std::ostream & oss
+		)
+	{
+		// [DoxyExample00]
+
+		// create a null instance
+		quadloco::fnd::ParmAD const aNull{};
+		bool const expValid{ false };
+		bool const gotValid{ isValid(aNull) };
+
+		// orthogonal line direction (e.g. lineDir is 1/4 RH turn from gradDir)
+		quadloco::pix::Grad const gradDir{ 1., 0. };
+		quadloco::dat::Vec2D<double> const expLineDir{ 0., 1. }; // RH 1/4 turn
+		quadloco::dat::Vec2D<double> const gotLineDir
+			{ quadloco::fnd::ParmAD::lineDirFromEdgeDir(gradDir) };
+
+		// [DoxyExample00]
+
+		if (! (gotValid == expValid))
+		{
+			oss << "Failure of aNull validity test(0)\n";
+			oss << "exp: " << expValid << '\n';
+			oss << "got: " << gotValid << '\n';
+		}
+
+		if (! nearlyEquals(gotLineDir, expLineDir))
+		{
+			oss << "Failure of gotLineDir test(0)\n";
+			oss << "exp: " << expLineDir << '\n';
+			oss << "got: " << gotLineDir << '\n';
+		}
+
+	}
+
+	//! Examples for documentation
+	void
+	test1
+		( std::ostream & oss
+		)
+	{
+		// [DoxyExample01]
+
+		// bounding circle
+		quadloco::dat::Spot const center{ 10., 20. };
+		constexpr double radius{ 10. };
+		quadloco::dat::Circle const circle{ center, radius };
+
+		// raster edge element
+		using namespace quadloco::pix;
+		Spot const pixLoc{ 10.f, 21.f }; // shift in col has no effect
+		Grad const pixGrad{ 1.f, 0.f }; // grad in row dir
+		Edgel const edgel{ pixLoc, pixGrad };
+
+		// the line segmeng of interest is perpendicular to edge gradient
+		quadloco::dat::Vec2D<double> const lineDir
+			{ quadloco::fnd::ParmAD::lineDirFromEdgeDir(edgel.gradient()) };
+
+		// the Alpha,Delta parameters associate with the line segment
+		static const double piHalf{ 2.*std::atan(1.) };
+		static const double pi{ 2.*piHalf };
+		quadloco::fnd::ParmAD const expParmAD{ -piHalf, pi };
+		quadloco::fnd::ParmAD const gotParmAD
+			{ quadloco::fnd::ParmAD::from(edgel, circle) };
+
+
+		// [DoxyExample01]
+
+		if (! nearlyEquals(gotParmAD, expParmAD))
+		{
+			oss << "Failure of gotParmAD test(1)\n";
+			oss << "exp: " << expParmAD << '\n';
+			oss << "got: " << gotParmAD << '\n';
+		}
+
+	}
+
+}
+
+//! Standard test case main wrapper
+int
+main
+	()
+{
+	int status{ 1 };
+	std::stringstream oss;
+
+	test0(oss);
+	test1(oss);
+
+	if (oss.str().empty()) // Only pass if no errors were encountered
+	{
+		status = 0;
+	}
+	else
+	{
+		// else report error messages
+		std::cerr << "### FAILURE in test file: " << __FILE__ << std::endl;
+		std::cerr << oss.str();
+	}
+	return status;
+}
+
