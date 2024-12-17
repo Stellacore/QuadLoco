53af09f6ae78a1c022127e22edb27d2bb27db016
Stellacore <dave@stellacore.com>
2024-12-16 17:29:19 -0700

    include/sigutil.hpp - add 'ignore input value' in toFloat(), add crude toSmooth() function

--- include/sigutil.hpp
+++ include/sigutil.hpp
@@ -61,6 +61,9 @@ namespace util
 	ras::Grid<float>
 	toFloat
 		( ras::Grid<uint8_t> const & uGrid
+			//!< Input source grid
+		, int const & treatAsNull = -1
+			//!< If value in range [0,255], then corresponding output is null
 		)
 	{
 		ras::Grid<float> fGrid(uGrid.hwSize());
@@ -68,10 +71,76 @@ namespace util
 		ras::Grid<float>::iterator outIter{ fGrid.begin() };
 		while (uGrid.cend() != inIter)
 		{
-			*outIter++ = *inIter++;
+			uint8_t const & inVal = *inIter;
+			float rtVal{ static_cast<float>(inVal) };
+			bool const checkForNull{ ! (treatAsNull < 0) };
+			if (checkForNull)
+			{
+				if (treatAsNull == inVal)
+				{
+					rtVal = std::numeric_limits<float>::quiet_NaN();
+				}
+			}
+			*outIter = rtVal;
+			inIter++;
+			outIter++;
 		}
 		return fGrid;
 	}
+
+	//! A Larger grid produced with (nearest neighbor) up sampling.
+	inline
+	ras::Grid<float>
+	toSmooth
+		( ras::Grid<float> const & inGrid
+//		, std::size_t const & wHalf
+		)
+	{
+		ras::Grid<float> outGrid(inGrid.hwSize());
+		constexpr float nan{ std::numeric_limits<double>::quiet_NaN() };
+		std::fill(outGrid.begin(), outGrid.end(), nan);
+
+		constexpr int wHalf{ 1 };
+		constexpr int wSize{ 2*wHalf + 1 };
+		ras::Grid<float> wgts(wSize, wSize);
+		wgts(0,0) = .5f;
+		wgts(0,1) = .8f;
+		wgts(0,2) = .5f;
+		wgts(1,0) = .8f;
+		wgts(1,1) = 1.f;
+		wgts(1,2) = .8f;
+		wgts(2,0) = .5f;
+		wgts(2,1) = .8f;
+		wgts(2,2) = .5f;
+		float const sumWgts{ 4.f*.5f + 4.f*.8f + .1f };
+
+		int const inRowEnd{ static_cast<int>(inGrid.high()) - wHalf };
+		int const inColEnd{ static_cast<int>(inGrid.wide()) - wHalf };
+		for (int inRow{wHalf} ; inRow < inRowEnd ; ++inRow)
+		{
+			int const inRow0{ inRow - wHalf };
+			for (int inCol{wHalf} ; inCol < inColEnd ; ++inCol)
+			{
+				int const inCol0{ inCol - wHalf };
+
+				// window processing
+				float sumVals{ 0.f };
+				for (int wRow{0} ; wRow < 2*wHalf ; ++wRow)
+				{
+					int const inRow{ inRow0 + wRow };
+					for (int wCol{0} ; wCol < 2*wHalf ; ++wCol)
+					{
+						int const inCol{ inCol0 + wCol };
+						sumVals += wgts(wRow, wCol) * inGrid(inRow, inCol);
+					}
+				}
+				outGrid(inRow, inCol) = sumVals / sumWgts;
+
+			}
+		}
+		return outGrid;
+	}
+
 
 	//! A Larger grid produced with (nearest neighbor) up sampling.
 	template <typename PixType>
