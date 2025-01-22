80081f8413a11b21c6da7f4b9aabf0ee8e51fad0
Stellacore <dave@stellacore.com>
2025-01-22 10:35:08 -0700

    include/opsCenterRefinerSSD.hpp add estimation of uncertainty in ssd minimum computation

--- include/opsCenterRefinerSSD.hpp
+++ include/opsCenterRefinerSSD.hpp
@@ -33,6 +33,7 @@
 
 
 #include "cast.hpp"
+#include "imgHit.hpp"
 #include "imgSpot.hpp"
 #include "prbStats.hpp"
 #include "rasGrid.hpp"
@@ -52,13 +53,19 @@ namespace ops
 	//! \brief Center finding functions assocaited with an external source grid
 	class CenterRefinerSSD
 	{
-		// access into external source grid
+		//! Access into external source grid
 		ras::Grid<float> const * const thePtSrcGrid{ nullptr };
 
+		//! Neighborhood half-size around candidate center location
 		std::size_t const theHalfHood{};
+
+		//! Filter half-size for rotation symmetry evaluation
 		std::size_t const theHalfCorr{};
 
+		//! Relative (signed) indices relative to candidate center peak
 		std::vector<ras::RelRC> theHoodRelRCs{};
+
+		//! Relative (signed) indices defining rotation symmetry filter inputs
 		std::vector<ras::RelRC> theCorrRelRCs{};
 
 		//! Relative row,col offsets for defining evaluation box
@@ -101,7 +108,7 @@ namespace ops
 			, std::size_t const & halfHood = 2u
 				//!< Half size of (2u*halfHood+1) neighborhood to search
 			, std::size_t const & halfCorr = 5u
-				//!< Radius of filter to use over all box cells
+				//!< Radius of rotation filter to use over all box cells
 			)
 			: thePtSrcGrid{ ptSrcGrid }
 			, theHalfHood{ halfHood }
@@ -135,15 +142,16 @@ namespace ops
 				ras::RowCol const rcHood0
 					{ hoodRelRC.srcRowCol(rcHoodCenterInSrc) };
 
-				// Use two iterators running from opposite directions
-				// to provide half-turn filter geometry
+				// Evaluate rotation symmetry at each point in evaluation
+				// neighborhood.  Use two iterators running from opposite
+				// directions to provide half-turn filter geometry
 				std::size_t const halfCorr{ theCorrRelRCs.size() / 2u };
 				FwdIter const fwdHalf{ theCorrRelRCs.cbegin() + halfCorr };
 				FwdIter fwdIter{theCorrRelRCs.cbegin()};
 				RevIter revIter{theCorrRelRCs.crbegin()};
 
-				// computing SSD over half-turn symmetric filter window
-				double sum{ 0. };
+				// compute filter response at this evaluation location
+				double sumSqDif{ 0. };
 				for ( ; fwdHalf != fwdIter ; ++fwdIter, ++revIter)
 				{
 					ras::RowCol const fwdRowCol
@@ -155,58 +163,99 @@ namespace ops
 					float const & revSrcVal = srcGrid(revRowCol);
 					double const diff
 						{ static_cast<double>(fwdSrcVal - revSrcVal) };
-					sum += diff*diff;
+					sumSqDif += diff*diff;
 				}
-				*outCorrIter++ = sum;
+				*outCorrIter++ = sumSqDif;
 
 			} // hood locations
 
 			return ssdGrid;
 		}
 
-		//! A sub-cell estimate of location for minimum
+		//! A sub-cell estimate of location for minimum within ssdGrid
 		inline
 		static
-		img::Spot
-		spotAtMinSSD
-			( ras::Grid<double> const & ssdGrid
+		img::Hit
+		hitAtMinimumOf
+			( ras::Grid<double> const & inGrid
 			)
 		{
-			img::Spot minSpot{};
-			prb::Stats<double> const ssdStats(ssdGrid.cbegin(), ssdGrid.cend());
-			val::Span const ssdSpan{ ssdStats.min(), ssdStats.max() };
+			img::Hit minHit{};
+			// gather stats over entire input grid
+			prb::Stats<double> const ssdStats(inGrid.cbegin(), inGrid.cend());
+			// use theoretical min value of zero and sample max value
+			// so that computed pseudo probabilities have meaning relative
+			// to theoretical perfect fit (the 0 value).
+			val::Span const ssdSpan{ 0., ssdStats.max() };
 			if (ssdSpan.isValid())
 			{
+				ras::Grid<double> probGrid(inGrid.hwSize());
+				std::fill(probGrid.begin(), probGrid.end(), 0.);
+
+				// compute weighted minimum over all elements of input grid
+				// (using a pseudo-probability weighting function to favor
+				// smaller values).
 				img::Vector<double> sumVec{ 0., 0. };
 				double sumProb{ 0. };
 				for (ras::Grid<double>::const_iterator
-					iter{ssdGrid.cbegin()} ; ssdGrid.cend() != iter ; ++iter)
+					iter{inGrid.cbegin()} ; inGrid.cend() != iter ; ++iter)
 				{
 					double const & ssdValue = *iter;
 					if (engabra::g3::isValid(ssdValue))
 					{
+						ras::RowCol const inRC{ inGrid.rasRowColFor(iter) };
+						img::Spot const locSpot{ cast::imgSpot(inRC) };
+
 						double const frac{ ssdSpan.fractionAtValue(ssdValue) };
 						double const arg{ 4. * frac };
 						double const prob{ std::exp(-arg*arg) };
-						img::Spot const locSpot
-							{ cast::imgSpot(ssdGrid.rasRowColFor(iter)) };
+
 						sumVec = sumVec + prob * locSpot;
 						sumProb += prob;
+
+						// Cache probability values to compute variance below
+						probGrid(inRC) = prob;
 					}
 				}
 				if (0. < sumProb)
 				{
 					// The SSD computations are based on matching areas
 					// of grid cells and the weighted average computed
-					// here is related to the cnter of the cells.
+					// here is related to the center of the cells.
 					// Therefore, add a half cell to recognize this
 					img::Vector<double> const halfCell{ .5, .5 };
 					img::Vector<double> const aveVec
 						{ (1./sumProb) * sumVec  + halfCell };
-					minSpot = cast::imgSpot(aveVec);
+					img::Spot const minSpot{ cast::imgSpot(aveVec) };
+
+					// Estimate variance
+					// TODO pbly could optimize to loop only near to minSpot
+					double sumSq{ 0. };
+					double sumProb{ 0. };
+					for (ras::Grid<double>::const_iterator
+						iter{inGrid.cbegin()} ; inGrid.cend() != iter ; ++iter)
+					{
+						double const & ssdValue = *iter;
+						if (engabra::g3::isValid(ssdValue))
+						{
+							ras::RowCol const inRC{ inGrid.rasRowColFor(iter) };
+							img::Spot const locSpot{ cast::imgSpot(inRC) };
+							img::Spot const relSpot{ locSpot - minSpot };
+							double const relMag{ magnitude(relSpot) };
+
+							double const & prob = probGrid(inRC);
+							sumSq += prob * relMag;
+							sumProb += prob;
+						}
+					}
+					double const var{ sumSq / sumProb };
+					double const sigma{ std::sqrt(var) };
+					double const probAtMin
+						{ probGrid(cast::rasRowCol(minSpot)) };
+					minHit = img::Hit(minSpot, probAtMin, sigma);
 				}
 			}
-			return minSpot;
+			return minHit;
 		}
 
 
@@ -217,18 +266,19 @@ namespace ops
 		 * the peak response from this filter.
 		 */
 		inline
-		img::Spot
-		fitSpotNear
+		img::Hit
+		fitHitNear
 			( ras::RowCol const & rcHoodCenterInSrc
 				//!< Center of window in which to search
 			) const
 		{
-			img::Spot fitSpot{};
+			img::Hit fitHit{};
 			std::size_t maxRad{ theHalfHood + theHalfCorr };
 
 			std::size_t const rowHood0{ rcHoodCenterInSrc.row() };
 			std::size_t const colHood0{ rcHoodCenterInSrc.col() };
 
+			// check if neighborhood filter size fits within source grid
 			bool isInterior{ false };
 			if ( thePtSrcGrid
 			  && (2u*maxRad < thePtSrcGrid->high())
@@ -254,10 +304,12 @@ namespace ops
 					{ ssdHoodGrid(rcHoodCenterInSrc) };
 
 				// estimate sub-cell location of minimum
-				img::Spot const minSpotInGrid{ spotAtMinSSD(ssdGrid) };
+				img::Hit const minHitInGrid
+					{ hitAtMinimumOf(ssdGrid) };
+				img::Spot const & minSpotInGrid = minHitInGrid.location();
 
 				// adjust ssdGrid location into full source image
-				fitSpot = img::Spot
+				img::Spot const fitSpot
 					{ minSpotInGrid.row()
 						+ (double)(rcHoodCenterInSrc.row())
 						- (double)(theHalfHood)
@@ -265,9 +317,13 @@ namespace ops
 						+ (double)(rcHoodCenterInSrc.col())
 						- (double)(theHalfHood)
 					};
+
+				double const & prob = minHitInGrid.value();
+				double const & sigma = minHitInGrid.sigma();
+				fitHit = img::Hit(fitSpot, prob, sigma);
 			}
 
-			return fitSpot;
+			return fitHit;
 		}
 
 	}; // CenterRefinerSSD
